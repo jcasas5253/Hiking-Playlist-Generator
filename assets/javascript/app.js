c14928554b57a0842da3f0d252bac6134dd6fb6a
skedabble <skedabble@gmail.com>
2020-03-18 15:45:09 -0700

    working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,237 @@
-$.fatNav();
+$(document).ready(function () {
+
+    $("#search-section").hide();
+    $("#search-section").fadeIn(1600);
+
+    // Initialize Modal
+    $('.modal').modal({ endingTop: '45%' });
+
+    $("#navBar").hide();
+    $("#trail-show").hide();
+
+    // Initialize Playlists
+    var easy = ["out-walking/pl.53385af769204a0ab843ce245f2fb293", "hiking-music/pl.7d96545f7c794d76b115ae9a920610cf", "hiking-music/pl.u-oZylKAaI0XpYmD"];
+    var medium = ["pure-workout/pl.ad0ee1557e3e4feba314fd70f7982766", "hiking-music/pl.u-2aoq8ZYSzqleym", "hiking-music/pl.u-PDb4zlpsyDD5a3"];
+    var hard = ["power-workout/pl.edc6571c1a5b49b1a8513673deaf18f5", "outdoors/pl.u-Ymb0vVPUxjXGeq", "outdoors/pl.u-DdANrVqsmllNJp"];
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDkM0haGwLMjz4QlsMomXfhLq2doAbOuX8",
+        authDomain: "aka-joe-project1.firebaseapp.com",
+        databaseURL: "https://aka-joe-project1.firebaseio.com",
+        projectId: "aka-joe-project1",
+        storageBucket: "aka-joe-project1.appspot.com",
+        messagingSenderId: "644601161185"
+    };
+    firebase.initializeApp(config);
+
+    // Create a variable to reference the database.
+    var database = firebase.database();
+    var favID = [];
+    var favCounter = [];
+    var favorite = [];
+    var results;
+
+    // Firebase watcher + initial loader
+    database.ref().on("value", function (snapshot) {
+        if (snapshot.exists()) {
+            // storing the snapshot.val() in a variable for convenience
+            favID = snapshot.val().ID;
+            favCounter = snapshot.val().counter;
+        };
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+
+    //Generate Playlist On Click
+    $(document).on("click", ".genBtn", function () {
+        var itunesURL = "https://embed.music.apple.com/us/playlist/";
+        var player = $("<iframe>").attr({
+            "allow": "autoplay *; encrypted-media *;",
+            frameborder: 0
+        })
+        var trackNo = Math.floor(Math.random() * 3);
+
+        if (this.value === "green" || this.value === "greenBlue") {
+            player.attr("src", itunesURL + easy[trackNo]);
+        } else if (this.value === "blue") {
+            player.attr("src", itunesURL + medium[trackNo]);
+        } else {
+            player.attr("src", itunesURL + hard[trackNo]);
+        }
+
+        $("#music-show").empty().append(player);
+        $(".trails").hide();
+        $(this).text("Close Playlist").addClass("closeBtn").removeClass("genBtn");
+        $($("#t" + $(this).attr("data"))).fadeIn();
+        $("#music-show").fadeIn();
+    });
+
+    //Closing Playlist Button
+    $(document).on("click", ".closeBtn", function () {
+        $(this).text("Generate Playlist").removeClass("closeBtn").addClass("genBtn");
+        $("#music-show").fadeOut();
+        $(".trails").fadeIn();
+    });
+
+    // Favorite button
+    $(document).on("click", ".favBtn", function () {
+
+        var newID = $(this).attr("value");
+        var position = favID.indexOf(newID);
+
+        // Check how many times the button has been clicked
+        if (position === -1) {
+            favorite.push(newID);
+            favID.push(newID);
+            favCounter.push(1);
+            $(this).text("♥ Liked!");
+            $("#" + newID).text("1 people liked this trail")
+        } else {
+            var position2 = favorite.indexOf(newID);
+            if (position2 === -1) {
+                favorite.push(newID);
+                favCounter[position]++;
+                $(this).text("♥ Liked!");
+                $("#" + newID).text(favCounter[position] + " people liked this trail");
+            } else {
+                favorite.splice(position2, 1);
+                favCounter[position]--;
+                $(this).text("♡ Like");
+                $("#" + newID).text(favCounter[position] + " people liked this trail");
+                if (favCounter[position] === 0) {
+                    favID.splice(position, 1);
+                    favCounter.splice(position, 1);
+                    $("#" + newID).empty();
+                };
+            };
+        };
+
+        // Code for "Setting values in the database"
+        database.ref().set({
+            ID: favID,
+            counter: favCounter
+        });
+    });
+
+    // Search bar
+    $(".searchBtn").on("click", function () {
+        event.preventDefault();
+        geoCodingApi($("#search2").val().trim());
+        $("header").hide();
+    });
+
+    // If 'Enter' key has been pressed
+    $("#search").on("keypress", function (e) {
+        if (e.which === 13) {
+            //Disable textbox to prevent multiple submit
+            $(this).attr("disabled", "disabled");
+
+            geoCodingApi($("#search").val().trim());
+
+            //Enable the textbox again if needed.
+            $(this).removeAttr("disabled");
+        }
+    });
+
+    // Using GeoCoding API to convert location data to logitude and latitude
+    function geoCodingApi(zip) {
+        var lat;
+        var lng;
+
+        var geocodingUrl = "https://maps.googleapis.com/maps/api/geocode/json?&address=";
+        var googleAPIkey = "AIzaSyAchT4KqcqtxBCYWoL7CkmhQ51Y2lGYhwg";
+
+        $.ajax({
+            type: "GET",
+            url: geocodingUrl + zip + "&key=" + googleAPIkey,
+            dataType: "json"
+        }).then(function (data) {
+            if (data.results.length === 0) {
+                // Cannot find the location
+                $('#modal1').modal('open');
+            } else {
+                lat = data.results[0].geometry.location.lat;
+                lng = data.results[0].geometry.location.lng;
+
+                $("#navBar").fadeIn();
+                $("#trail-show").empty().fadeIn().html("<div id='music-show' class='col l6'></div>");
+                $("#music-show").fadeOut();
+                $("#search-section").fadeOut();
+
+                hikeApi(lat, lng);
+            };
+        });
+    };
+
+    // Using Hiking API to get info about Hiking trails
+    function hikeApi(lat, lon) {
+
+        var trailKey = "200706955-f6caa0e92a89cd162c8dac182001caf2";
+        var queryURL = "https://www.hikingproject.com/data/get-trails?" + "lat=" + lat + "&lon=" + lon + "&maxDistance=10" + "&key=" + trailKey;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            results = response.trails;
+            console.log(results);
+            displayTrails();
+        });
+    };
+
+    // Display trails info
+    function displayTrails() {
+        for (var i = 0; i < results.length; i++) {
+            var trailDiv = $("<div class='trails col l6'>").attr("id", "t" + results[i].id);
+
+            var n = $("<h5>").text(results[i].name);
+            var l = $("<p>").text("Location: " + results[i].location);
+            var c = $("<p>").text("Condition: " + results[i].conditionStatus);
+            var d = $("<p>").text("Difficulty: " + results[i].difficulty);
+            var le = $("<p>").text("Length: " + results[i].length);
+            var s = $("<p>").text("Stars: " + results[i].stars);
+
+            var bn = $("<button>Generate Playlist</button>").attr({
+                data: results[i].id,
+                class: "btn genBtn waves-effect waves-light",
+                value: results[i].difficulty,
+            })
+
+            var newID = results[i].id.toString();
+
+            var b = $("<div>").attr({ class: "btn favBtn waves-effect waves-light red", value: results[i].id })
+            if (favorite.indexOf(newID) === -1) {
+                b.text("♡ Like")
+            } else {
+                b.text("♥ Liked!")
+            };
+
+            var f = $("<div>").attr("id", results[i].id);
+            var position = favID.indexOf(newID);
+            if (position != -1) {
+                f.text(favCounter[position] + " people liked this trail")
+            };
+
+            var imgLink = $("<a>");
+            imgLink.attr({ href: results[i].url, target: "_blank" });
+
+            var image = $("<img>");
+            if (results[i].imgSmallMed === "") {
+                image.attr("src", "./assets/images/no_image.jpg");
+            } else {
+                image.attr("src", results[i].imgSmallMed);
+            };
+            image.addClass("image");
+            imgLink.append(image);
+
+            trailDiv.append(imgLink);
+            trailDiv.append(n, l, c, d, le, s, b, bn, f);
+
+            $("#trail-show").prepend(trailDiv);
+            trailDiv.hide();
+            trailDiv.fadeIn(200 * i);
+        };
+    };
+});
